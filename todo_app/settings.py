8e1cea92a6ff1929670e2a835df4977747ea8041
Israel Ebonko <ebonko2017ip@gmail.com>
2019-09-09 14:40:45 +0100

    use default db sqlite

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tododb',
-        'USER':  'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306'    
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
