22481af4d9dba61afeca4a15c7f01b563c56f090
Israel Ebonko <ebonko2017ip@gmail.com>
2019-09-11 05:16:54 +0100

    add undo complete button

--- todo/urls.py
+++ todo/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
 			path('<task_id>/actioned', views.actioned_task, name='actioned_task'),
 			path('<task_id>/delete', views.delete_task, name='delete_task'),
 			path('<int:id>/edit/', views.edit_task, name='edit_task'),
-			path('<task_id>/undo_actioned', views.undo_actioned_task, name='actioned_task'),
+			path('<task_id>/undo_actioned', views.undo_actioned_task, name='undo_actioned_task'),
 	]
