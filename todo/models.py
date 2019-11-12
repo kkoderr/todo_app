e7cc2104467a813b9c1749b6cc675bf11f7d73f1
Israel Ebonko <ebonko2017ip@gmail.com>
2019-11-12 09:57:54 +0100

    changed model structure

--- todo/models.py
+++ todo/models.py
@@ -1,25 +1,18 @@
 from django.db import models
 from django.urls import reverse
 
-#  Model for different task fields.
-
-CATEGORY = (
-	('None','None'),
-	('Work','Work'), 
-	('Personal','Personal'), 
-	('Study','Study'), 
-	('Research','Research')
-	)
 
 class Task(models.Model):
+	"""The Task table fields """
+
 	task = models.CharField(max_length=100, verbose_name= 'New Task')
 	date_created = models.DateTimeField(auto_now_add=True)
-	category = models.CharField(max_length=100, choices=CATEGORY, default='none', verbose_name='Choose Category')
+	none = models.BooleanField(default=False)
+	work = models.BooleanField(default=False)
+	personal = models.BooleanField(default=False)
+	study = models.BooleanField(default=False)
+	research = models.BooleanField(default=False)
 	actioned = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.task
-
-	def get_absolute_url(self):
-		return reverse('todo:edit_task', kwargs={"id": self.id})
-		#return reverse('todo:TaskListView')
